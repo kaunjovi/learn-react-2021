058da2fd3190de1012c7cb4305f4424fd224a48a
kaun jovi <kaunjovi@gmail.com>
2021-03-21 20:38:54 +0530

    Got REACT to work

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,25 @@ module.exports = {
 
     mode:'development',
 
+    entry : './src/index.js',
+
+    output : {
+        path : path.resolve(__dirname , 'dist'),
+        filename: 'index_bundle.js'
+    },
+
+    module : {
+
+        rules : [
+            {test : /\.(js)$/, use:'babel-loader'},
+        ]
+
+    },
+
     plugins : [
-
         new HtmlWebpackPlugin ({
-
-            template : 'app/index.html'
-
+            template : 'src/index.html'
         })
-
     ]
 
 }
